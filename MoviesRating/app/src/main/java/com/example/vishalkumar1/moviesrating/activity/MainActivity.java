6c071c3fa843bdfd5069469712cdd5262ab739a3
Devil29 <vishalraj48@gmail.com>
2016-08-18 16:52:51 +0530

    Mpvie rating completed

--- MoviesRating/app/src/main/java/com/example/vishalkumar1/moviesrating/activity/MainActivity.java
+++ MoviesRating/app/src/main/java/com/example/vishalkumar1/moviesrating/activity/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.vishalkumar1.moviesrating.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.widget.Toast;
 
 import com.example.vishalkumar1.moviesrating.R;
+import com.example.vishalkumar1.moviesrating.adapter.MoviesAdapter;
 import com.example.vishalkumar1.moviesrating.constant.ProjectConst;
 import com.example.vishalkumar1.moviesrating.model.Movie;
 import com.example.vishalkumar1.moviesrating.model.MovieResponse;
@@ -30,12 +33,17 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(),"Get a API Key",Toast.LENGTH_LONG).show();;
             return;
         }
+
+        final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.movies_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
         ApiInterface apiService = ApiClient.getClient().create(ApiInterface.class);
         Call<MovieResponse> call= apiService.getTopRatedMovies(API_KEY);
         call.enqueue(new Callback<MovieResponse>() {
             @Override
             public void onResponse(Call<MovieResponse> call, Response<MovieResponse> response) {
                 List<Movie> movies=response.body().getResults();
+                recyclerView.setAdapter(new MoviesAdapter(movies, R.layout.list_item_movie, getApplicationContext()));
                 Log.d("Number of Movies", "" + movies.size());
             }
             @Override
