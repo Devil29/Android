a0ce2b8791f8bacf663c95c8ae3cfaefd91afff4
Devil29 <vishalraj48@gmail.com>
2016-08-18 16:06:26 +0530

    Added Recycler Viewer

---
+++ MoviesRating/app/src/main/java/com/example/vishalkumar1/moviesrating/adapter/MoviesAdapter.java
@@ -0,0 +1,81 @@
+package com.example.vishalkumar1.moviesrating.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.vishalkumar1.moviesrating.R;
+import com.example.vishalkumar1.moviesrating.model.Movie;
+
+import java.util.List;
+
+/**
+ * Created by vishal.kumar1 on 18/08/16.
+ */
+public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MovieViewHolder> {
+
+    private List<Movie> movies;
+    private int rowLayout;
+    private Context context;
+
+    public static  class MovieViewHolder extends RecyclerView.ViewHolder{
+        LinearLayout moviesLayout;
+        TextView movieTitle;
+        TextView data;
+        TextView movieDescription;
+        TextView rating;
+
+        public MovieViewHolder(View v){
+            super(v);
+            moviesLayout = (LinearLayout) v.findViewById(R.id.movies_layout);
+            movieTitle = (TextView) v.findViewById(R.id.title);
+            data = (TextView) v.findViewById(R.id.subtitle);
+            movieDescription = (TextView) v.findViewById(R.id.description);
+            rating = (TextView) v.findViewById(R.id.rating);
+        }
+    }
+
+    public MoviesAdapter(List<Movie> movies, int rowLayout, Context context) {
+        this.movies = movies;
+        this.rowLayout = rowLayout;
+        this.context = context;
+    }
+
+    @Override
+    public MoviesAdapter.MovieViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(rowLayout, parent, false);
+        return new MovieViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MoviesAdapter.MovieViewHolder holder, int position) {
+        holder.movieTitle.setText(movies.get(position).getTitle());
+        holder.data.setText(movies.get(position).getReleaseDate());
+        holder.movieDescription.setText(movies.get(position).getOverview());
+        holder.rating.setText(movies.get(position).getVoteAverage().toString());
+    }
+
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
