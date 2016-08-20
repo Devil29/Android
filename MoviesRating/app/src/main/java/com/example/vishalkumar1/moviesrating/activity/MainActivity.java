5700d5c5fdc9166e210c6e8069871f33ae1281b2
Devil29 <vishalraj48@gmail.com>
2016-08-21 00:08:15 +0530

    OnclickListener For RecyclerView in MovieList

--- MoviesRating/app/src/main/java/com/example/vishalkumar1/moviesrating/activity/MainActivity.java
+++ MoviesRating/app/src/main/java/com/example/vishalkumar1/moviesrating/activity/MainActivity.java
@@ -5,11 +5,13 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import com.example.vishalkumar1.moviesrating.R;
 import com.example.vishalkumar1.moviesrating.adapter.MoviesAdapter;
 import com.example.vishalkumar1.moviesrating.constant.ProjectConst;
+import com.example.vishalkumar1.moviesrating.listener.RecyclerTouchListener;
 import com.example.vishalkumar1.moviesrating.model.Movie;
 import com.example.vishalkumar1.moviesrating.model.MovieResponse;
 import com.example.vishalkumar1.moviesrating.rest.ApiClient;
@@ -24,6 +26,7 @@ import retrofit2.Response;
 public class MainActivity extends AppCompatActivity {
 
     private static final String API_KEY= ProjectConst.Api_Key;
+    List<Movie> movies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,13 +39,25 @@ public class MainActivity extends AppCompatActivity {
 
         final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.movies_recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.addOnItemTouchListener(new RecyclerTouchListener(getApplicationContext(), recyclerView, new ClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                Movie movie = movies.get(position);
+                Toast.makeText(getApplicationContext(), movie.getTitle() + " is selected!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onLongClick(View view, int position) {
+
+            }
+        }));
 
         ApiInterface apiService = ApiClient.getClient().create(ApiInterface.class);
         Call<MovieResponse> call= apiService.getTopRatedMovies(API_KEY);
         call.enqueue(new Callback<MovieResponse>() {
             @Override
             public void onResponse(Call<MovieResponse> call, Response<MovieResponse> response) {
-                List<Movie> movies=response.body().getResults();
+                movies=response.body().getResults();
                 recyclerView.setAdapter(new MoviesAdapter(movies, R.layout.list_item_movie, getApplicationContext()));
                 Log.d("Number of Movies", "" + movies.size());
             }
@@ -51,5 +66,11 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("Error" , "Error");
             }
         });
+    }
+
+    public interface ClickListener {
+        void onClick(View view, int position);
+
+        void onLongClick(View view, int position);
     }
 }
