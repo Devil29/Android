39dfbd76a6e4999aa56ec9b2d70cc2e82d505815
Devil29 <vishalraj48@gmail.com>
2016-08-22 13:14:07 +0530

    SplashActivity Has been Added

---
+++ MoviesRating/app/src/main/java/com/example/vishalkumar1/moviesrating/activity/SplashActivity.java
@@ -0,0 +1,37 @@
+package com.example.vishalkumar1.moviesrating.activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+import com.example.vishalkumar1.moviesrating.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        textView = (TextView) findViewById(R.id.text);
+        Thread timer = new Thread() {
+            public void run() {
+                try {
+                    Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade);
+                    textView.startAnimation(animation);
+                    sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+        timer.start();
+    }
+}
